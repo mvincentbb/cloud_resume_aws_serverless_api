1ecb13f9f941a868cf9efce37051cfa1a80730ce
vincent <vincent.bakpatina@ipnetinstitute.com>
2022-12-28 17:12:49 +0000

    update gitignore

--- .aws-sam/build/VisitorFunction/handler.py
+++ .aws-sam/build/VisitorFunction/handler.py
@@ -1,6 +1,43 @@
-import json
+import json, boto3
+
+
 def handler(event, context):
     # Log the event argument for debugging and for use in local development.
     print(json.dumps(event))
 
-    return {}
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('challenge_DB')
+    response = {
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+
+    if str(event['routeKey']) == "GET /items/{id}":
+        item_id = event['pathParameters']['id']
+        result = table.get_item(
+            Key = {
+                'id':item_id
+            },
+        )
+        visitors = result['Item']['visitors']
+        response['body'] = json.dumps(int(visitors))
+        response['statusCode'] = 200
+
+    elif  event['routeKey'] == 'PUT /items':
+        request_body = json.loads(event['body'])
+        #Put item in DynamoDB
+        result = table.put_item(
+            Item={
+                'id' : request_body['id'],
+                'visitors' : request_body['visitors']
+            })
+        response['statusCode'] = 200
+        response['body'] = " update successfully "
+
+    else:
+        response['statusCode'] = 400
+        response['body'] = "error"
+
+    return response
